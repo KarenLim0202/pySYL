93b58c00e37bad7940df8ca892ac7fcc19448be9
541943197 <541943197@qq.com>
2017-11-01 21:28:03 +0800

    scrapy exercise1

---
+++ spider1/spider2.py
@@ -0,0 +1,12 @@
+import scrapy
+class ShiyanlouCoursesSpider(scrapy.Spider):
+    name='shiyanlou-courses'
+
+    @property
+    def start_urls(self):
+        url_tmpl='https://www.shiyanlou.com/courses/?category=all&course_type=all&fee=all&tag=all&page={}'
+        return (url_tmpl.format(i) for i in range(1,23))
+
+    
+    def parse(self,response):
+        for course in response.css()
